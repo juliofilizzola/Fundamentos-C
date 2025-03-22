85af3e375adb39edf70d1c18adec43f7ef503585
julio_filizzola <juliofilizzola@hotmail.com>
2025-03-22 14:32:30 -0300

    initialize CMake project structure and configuration files

---
+++ fundamento1.c
@@ -0,0 +1,23 @@
+//
+// Created by Julio on 22/03/2025.
+//
+#include <stdio.h>
+
+int main() {
+    int num1, num2, soma;
+
+    // Lendo dois números do usuário
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    // Calculando a soma dos dois números
+    soma = num1 + num2;
+
+    // Exibindo o resultado
+    printf("A soma de %d e %d é: %d\n", num1, num2, soma);
+
+    return 0;
+}
