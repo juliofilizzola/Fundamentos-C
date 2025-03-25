bc16b6bab9912b121407f5da8421092fc982cadb
Julio Filizzola <juliofilizzola@hotmail.com>
2025-03-25 09:54:52 -0300

    add program to calculate the sum of two numbers

---
+++ input-output/input.c
@@ -0,0 +1,15 @@
+//
+// Created by julio-filizzola on 25/03/25.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("Enter another number: ");
+    scanf("%d", &b);
+    printf("The sum of the numbers is %d\n", a + b);
+    return 0;
+}
