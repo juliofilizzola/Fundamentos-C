3d76ac45579b259fe4eb27015c0d3e8bf08f0d47
Julio Filizzola <juliofilizzola@hotmail.com>
2025-04-08 09:24:18 -0300

    feat: add program to demonstrate signed and unsigned integer behavior

---
+++ modify-type/modify-type.c
@@ -0,0 +1,15 @@
+//
+// Created by julio-filizzola on 08/04/25.
+//
+#include <stdio.h>
+
+int main() {
+
+    int signedNumber = 3000000000; // Este valor excede o limite de um int normal
+    unsigned int unsignedNumber = 3000000000;
+
+    printf("Número com sinal: %d\n", signedNumber);
+    printf("Número sem sinal: %u\n", unsignedNumber);
+
+  return 0;
+}
