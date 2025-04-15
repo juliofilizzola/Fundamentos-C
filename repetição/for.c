0369f089f64b6604acb59109ddfc80e8a9c571f5
Julio Filizzola <juliofilizzola@hotmail.com>
2025-04-15 08:56:23 -0300

    feat: add program to print numbers from 0 to 10 using a for loop

---
+++ repetição/for.c
@@ -0,0 +1,12 @@
+//
+// Created by julio-filizzola on 15/04/25.
+//
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i = 0; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
