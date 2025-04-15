e782e0143afdc75279e86ba47f8e1f3bb296cdf5
Julio Filizzola <juliofilizzola@hotmail.com>
2025-04-15 08:51:22 -0300

    feat: add program to print numbers from 0 to 10 using a while loop

---
+++ repetição/while.c
@@ -0,0 +1,14 @@
+//
+// Created by julio-filizzola on 15/04/25.
+//
+
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    while (i <= 10) {
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
