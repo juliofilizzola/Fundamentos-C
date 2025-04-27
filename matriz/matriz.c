7c68ccc58cb1494c0fbecc68f780afc43ab20644
julio_filizzola <juliofilizzola@hotmail.com>
2025-04-27 17:51:47 -0300

    Add initial matrix implementation files
    Introduce matriz.c and matriz.h as the foundation for matrix operations. This commit includes a simple main function with a predefined 3x3 matrix, but further functionality is yet to be implemented.

---
+++ matriz/matriz.c
@@ -0,0 +1,22 @@
+//
+// Created by Julio on 27/04/2025.
+//
+
+#include "matriz.h"
+
+#include <stdio.h>
+
+
+int main() {
+    // definindo exemplos praticos de  matrizes
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    // printf(matriz);
+    printf('xablau');
+
+    // Exibindo a matriz
+}
