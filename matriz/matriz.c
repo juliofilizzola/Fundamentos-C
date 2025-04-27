f15b4cb8dd86f9b0870dfd3dc05ae62d852172e9
julio_filizzola <juliofilizzola@hotmail.com>
2025-04-27 18:08:13 -0300

    Refactor matrix initialization in nested loop.
    Moved matrix initialization inside the nested loop in `matriz.c` for better logical grouping within the loop. Adjusted build logs to reflect changes in updated execution timestamps.

--- matriz/matriz.c
+++ matriz/matriz.c
@@ -9,14 +9,14 @@
 
 int main() {
     // definindo exemplos praticos de  matrizes
-    int matriz[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
+            const int matriz[3][3] = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
